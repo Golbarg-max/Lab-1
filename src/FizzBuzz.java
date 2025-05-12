1f1a3639079a526b2b5f269ed536bbef23d827a9
Golbarg-max <golbarg1384@gmail.com>
2025-05-12 16:32:45 -0400

    created two new files

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,13 +5,13 @@ class FizzBuzz {
 
     public static void main(String[] args) {
         int i = 1;
-        while (i < 100) {
+        while (i < 6) {
             i = doFizzBuzz(i);
+                    i++;
         }
     }
 
-    private static int doFizzBuzz(int i) {
-        i++;
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
