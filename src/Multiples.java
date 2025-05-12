aa614823c765fc0e72e875fc6061709031e2a656
Golbarg-max <golbarg1384@gmail.com>
2025-05-12 16:54:14 -0400

    implement Multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,13 @@
 public class Multiples {
    public static void main(String[] args) {
+       int ans = 0;
        for (int i = 1; i < 1000; i++){
-
+          boolean divisibleBy3 = i % 3 == 0;
+          boolean divisibleBy5 = i % 5 == 0;
+          if (divisibleBy3||divisibleBy5) {
+            ans++;
+          }
        }
+       System.out.println(ans);
    }
 }
